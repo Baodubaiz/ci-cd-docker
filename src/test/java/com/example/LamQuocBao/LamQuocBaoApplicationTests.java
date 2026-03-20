523a982584819e88986b9fbf46355421e2de4884
Baodubaiz <baolam12122004@gmail.com>
2026-03-20 15:53:39 +0700

    first commit

---
+++ src/test/java/com/example/LamQuocBao/LamQuocBaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LamQuocBao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LamQuocBaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
